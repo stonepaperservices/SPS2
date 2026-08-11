b901713c44ed5e173404bfc6c0d2a3aa7d2745d6
stonepaperservices <stonepaperservices@gmail.com>
2026-08-12 02:21:01 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
-  base: process.env.VITE_BASE || "/",
+  base: "/SPS2/",
   plugins: [react()],
   resolve: {
     alias: {
